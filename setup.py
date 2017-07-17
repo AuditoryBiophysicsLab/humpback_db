ead750f08690dcfd4bc9b59cbe7d89b177a40233
Graham Voysey <gvoysey@bu.edu>
2017-07-16 21:58:17 -0400

    updated and added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='humpback_db',
+        version='',
+        packages=find_packages(),
+        scripts=['humpback_db/scripts/humpbackdb'],
+        url='',
+        license='',
+        author='Graham Voysey',
+        author_email='gvoysey@bu.edu',
+        description=''
+)
