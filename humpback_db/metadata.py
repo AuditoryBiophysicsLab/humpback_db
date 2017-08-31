59ee29d33067e5c8dd67f9b96866e7f5d75f49ca
Graham Voysey <gvoysey@bu.edu>
2017-08-31 19:21:11 -0400

    validation OK

--- humpback_db/metadata.py
+++ humpback_db/metadata.py
@@ -4,14 +4,31 @@ import attr
 from attr.validators import instance_of
 from typing import Tuple, Optional
 import inspect
+from logging import info, error
 from enum import Enum
 from datetime import datetime
 from dateutil import parser
+
 
 class SideOfHead(Enum):
     RIGHT = 0
     LEFT = 1
 
+@attr.s
+class Point:
+    x = attr.ib()
+    y = attr.ib()
+
+@attr.s
+class ROI:
+    top_left = attr.ib(validator=instance_of(Point))
+    bottom_right =attr.ib(validator=instance_of(Point))
+
+    @property
+    def area(self):
+        height = self.top_left.y - self.bottom_right.y
+        width = self.top_left.x - self.bottom_right.x
+        return height*width
 
 @attr.s
 class Metadata:
@@ -22,15 +39,23 @@ class Metadata:
     depth = attr.ib(validator=instance_of(int))
     digitization_date = attr.ib(validator=instance_of(datetime))
 
+    def __attrs_post_init___(self):
+        self.roi = attr.ib()
+
 def get_id(s:bs4.element.Tag) -> str:
     """Get the animal ID string"""
-    return s.title.contents[0].split('_')[0].upper()
+    try:
+        return s.title.contents[0].split('_')[0].upper()
+    except AttributeError as a:
+        error(f'animal ID not found: \n\t{a}')
+        return None
 
 def get_notes(s:bs4.element.Tag) -> str:
     """Get any notes entered during scanning"""
     try:
         return s.comment.contents[0]
     except AttributeError as a:
+        error(f'notes not found: \n\t{a}')
         return None
 
 def get_side_of_head(s:bs4.element.Tag) -> SideOfHead:
@@ -42,6 +67,7 @@ def get_side_of_head(s:bs4.element.Tag) -> SideOfHead:
     elif 'lt' in title or 'left' in description:
         return SideOfHead.LEFT
     else:
+        error(f'side of head cannot be determined from:\n\ttitle:{title}\n\t description:{description}')
         return None
 
 def get_depth(s:bs4.element.Tag) -> int :
@@ -49,21 +75,26 @@ def get_depth(s:bs4.element.Tag) -> int :
     title = s.title.contents[0].lower()
     try:
         return int(title.split('_')[1])
-    except ValueError:
+    except ValueError as v:
+        error(f'could not determine depth from title:{title}: \n\t{v}')
         return None
 
 def get_digitization_date(s:bs4.element.Tag) -> datetime:
     """Get the date the slide was digitized"""
-    return parser.parse(s.acquisitiondateandtime.contents[0])
+    try:
+        datestr = s.acquisitiondateandtime.contents[0]
+        return parser.parse(datestr)
+    except:
+        error(f'could not determine datetime from str:{datestr}')
 
 
 
-def tryparse(metadata_file:str) -> Tuple[Optional[Metadata],bool, str]:
+def tryparse(metadata_file:str) -> Tuple[Optional[Metadata],bool]:
     s = bs(open(metadata_file, 'r').read(), 'lxml')
     document = s.information.document
     image = s.information.image
     del s
-    message = ''
+
     contents = {'id' : get_id(document)
                 , 'notes' : get_notes(document)
                 , 'side_of_head': get_side_of_head(document)
@@ -74,12 +105,12 @@ def tryparse(metadata_file:str) -> Tuple[Optional[Metadata],bool, str]:
         retval = Metadata(**contents)
     except TypeError as e:
         retval = None
-        message += f"could not extract metadata from {metadata_file}: {str(e)}"
-    return retval, message == '', message
+        error(f"could not extract metadata from {metadata_file}: {str(e)}")
+    return retval, retval == None
 
 
 
 
 if __name__ == "__main__":
-    meta, success, msg = tryparse('C:/Users/Graham Voysey/Downloads/PP21_321_Rt.tiff_metadata.xml')
+    meta, success = tryparse('C:/Users/Graham Voysey/Downloads/PP21_321_Rt.tiff_metadata.xml')
     print(meta)
